7e2b0c860b283901b9c35dd78b34189993c5db7b
Robert Schultz <robert@cosmicrealms.com>
2015-09-08 09:49:01 -0400

    Updated to latest nan 2.x version

--- rand48.cc
+++ rand48.cc
@@ -14,7 +14,7 @@ using namespace v8;
 class Rand48 : public node::ObjectWrap
 {
 	public:
-		static void Init(v8::Handle<v8::Object> exports);
+		static void Init(v8::Local<v8::Object> exports);
 		long int rand(long int min, long int max);
 
 	private:
@@ -52,31 +52,27 @@ long int Rand48::rand(long int min, long int max)
 	return (long int)(min + r / (NRAND48_MAX / ((max+1)-min) + 1));
 }
 
-void Rand48::Init(Handle<Object> exports)
+void Rand48::Init(Local<Object> exports)
 {
-	NanScope();
-
 	// Prepare constructor template
-	Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(New);
-	tpl->SetClassName(NanNew("Rand48"));
+	Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
+	tpl->SetClassName(Nan::New("Rand48").ToLocalChecked());
 	tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
 	// Prototype
-	NODE_SET_PROTOTYPE_METHOD(tpl, "rand", Rand);
+	Nan::SetPrototypeMethod(tpl, "rand", Rand);
 
-	NanAssignPersistent(constructor, tpl->GetFunction());
-	exports->Set(NanNew("Rand48"), tpl->GetFunction());
+	//Nan::AssignPersistent(constructor, tpl->GetFunction());
+	exports->Set(Nan::New("Rand48").ToLocalChecked(), tpl->GetFunction());
 }
 
 NAN_METHOD(Rand48::New)
 {
-	NanScope();
-
 	unsigned short int psubi[3];
 
-	if(args.IsConstructCall())
+	if(info.IsConstructCall())
 	{
-		if(args[0]->IsUndefined())
+		if(info[0]->IsUndefined())
 		{
 			struct timeval now;
 			gettimeofday(&now, 0);
@@ -86,46 +82,47 @@ NAN_METHOD(Rand48::New)
 		}
 		else
 		{
-			psubi[0] = args[0]->NumberValue();
-			psubi[1] = args[1]->IsUndefined() ? 0 : args[1]->NumberValue();
-			psubi[2] = args[2]->IsUndefined() ? 0 : args[2]->NumberValue();
+			psubi[0] = info[0]->NumberValue();
+			psubi[1] = info[1]->IsUndefined() ? 0 : info[1]->NumberValue();
+			psubi[2] = info[2]->IsUndefined() ? 0 : info[2]->NumberValue();
 		}
 
 		Rand48 * obj = new Rand48(psubi);
-		obj->Wrap(args.This());
-		NanReturnValue(args.This());
+		obj->Wrap(info.This());
+		info.GetReturnValue().Set(info.This());
 	}
 }
 
 NAN_METHOD(Rand48::Rand)
 {
-	NanScope();
-
-	if(args.Length()!=2)
+	if(info.Length()!=2)
 	{
-		NanThrowTypeError("Wrong number of arguments");
-    	NanReturnUndefined();
+		Nan::ThrowTypeError("Wrong number of arguments");
+    	info.GetReturnValue().Set(Nan::Undefined());
+    	return;
 	}
 
-	if(!args[0]->IsInt32() || !args[1]->IsInt32())
+	if(!info[0]->IsInt32() || !info[1]->IsInt32())
 	{
 		char buf[1024];
 		sprintf(buf, "rand(min, max) arguments must be between 0 and %u", NRAND48_MAX);
-		NanThrowTypeError(buf);
-    	NanReturnUndefined();
+		Nan::ThrowTypeError(buf);
+    	info.GetReturnValue().Set(Nan::Undefined());
+    	return;
 	}
 
-	if(args[1]->Int32Value()<args[0]->Int32Value())
+	if(info[1]->Int32Value()<info[0]->Int32Value())
 	{
-		NanThrowTypeError("rand(min, max) min MUST be greather than or equal to max");
-    	NanReturnUndefined();
+		Nan::ThrowTypeError("rand(min, max) min MUST be greather than or equal to max");
+    	info.GetReturnValue().Set(Nan::Undefined());
+    	return;
 	}
 
-	Rand48 * r = Rand48::Unwrap<Rand48>(args.Holder());
-	NanReturnValue(NanNew<v8::Int32>((int32_t)r->rand(args[0]->Int32Value(), args[1]->Int32Value())));
+	Rand48 * r = Rand48::Unwrap<Rand48>(info.Holder());
+	info.GetReturnValue().Set(Nan::New<v8::Int32>((int32_t)r->rand(info[0]->Int32Value(), info[1]->Int32Value())));
 }
 
-void Init(Handle<Object> exports, Handle<Object> module)
+void Init(Local<Object> exports)
 {
 	Rand48::Init(exports);
 }
--- test.js
+++ test.js
@@ -2,10 +2,9 @@
 var Rand48 = require("./index").Rand48;
 
 var r = new Rand48();   		// Creates a new Rand48 object, with no seed
-console.log(r.rand(1, 10));     // Requests a number between 1 and 10 inclusive
-
 var r2 = new Rand48(43912, 23555, 12660);     // Creates a new Rand48 object with a seed of 43912, 23555, 12660
-console.log(r2.rand(1, 10));     // Produces the number 10, random based on seed
-
 var r3 = new Rand48();
+
+console.log(r.rand(1, 10));     // Requests a number between 1 and 10 inclusive
+console.log(r2.rand(1, 10));     // Produces the number 10, random based on seed
 //console.log(r3.rand(0, -1));	// Should NOT cause a floating point exception, should throw an exception
